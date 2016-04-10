4c7077c712eb0a5b47a9a7d10694f07326d67af5
Koding <f.nasim01@gmail.com>
2016-04-10 16:51:17 +0000

    encapusulated operations into class

--- json_parsing.py
+++ json_parsing.py
@@ -1,11 +1,22 @@
 import json
 
-# read file
-json_file = open('data.json')
-file_content = json_file.read()
+class JsonParser:
+
+    def __init__(self):
+        pass
+    
+    def parse_json_string(self, json_str):
+        return json.loads(json_str)
+
+    def parse_json_file(self, file_path):
+        json_file = open(file_path)
+        file_content = json_file.read()
+        return self.parse_json_string(file_content)
+
 
 # parse values into dictionary
-value_dict = json.loads(file_content)
+parser = JsonParser()
+value_dict = parser.parse_json_file('data.json')
 
 # iterate over the dictionary
 for key in value_dict:
